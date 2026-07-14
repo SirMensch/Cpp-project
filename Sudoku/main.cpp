d50c4d53e1f15bc1f0b2d32def3ad13fc06a1854
SirMensch <larslue@googlemail.com>
2026-07-14 15:16:36 +0900

    RM: sudoku projects (empty)

--- Sudoku/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "header/SudokuField.h"
-
-int main() {
-    SudokuField field;
-    field.Print();
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
-}
--- Sudoku/src/SudokuGenerator.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by larsl on 6/27/2023.
-//
-
-#include "../header/SudokuGenerator.h"
