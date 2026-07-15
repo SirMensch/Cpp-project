8d1d195f29a3bdbc3f9536d3c82a151a7697df3a
SirMensch <larslue@googlemail.com>
2026-07-15 15:18:30 +0900

    ADD: jmp, call, ret

--- emulator-6502/src/cpu/cpu.cpp
+++ emulator-6502/src/cpu/cpu.cpp
@@ -1,5 +1,20 @@
 #include "cpu.h"
+#include <cstdint>
 #include <stdexcept>
+
+template <typename T> T extract_bits(T input, uint8_t start, uint8_t length) {
+  // compile time assess
+  constexpr uint8_t type_bits = sizeof(T) * 8;
+  static_assert(std::is_integral<T>::value, "Template type must be an integer");
+  static_assert(start < type_bits, "Start position exceeds data type size");
+  static_assert(length <= type_bits, "Length exceeds data type size");
+
+  if (start >= type_bits || length == 0) {
+    return 0;
+  }
+  T mask = (static_cast<T>(1) << length) - 1;
+  return (input >> start) & mask;
+}
 
 CPU::CPU() { reset(); }
 
@@ -18,4 +33,60 @@ void CPU::load_program(const uint8_t *program, std::size_t size) {
     throw std::runtime_error("Program size exceeds available memory.");
   }
   std::copy(program, program + size, memory_.begin() + PROGRAM_COUNTER_START);
+}
+
+void CPU::get_next_instruction() {
+  uint16_t instruction = 0x0;
+  instruction |= memory_[program_counter_++] << 8; // MSB
+  instruction |= memory_[program_counter_++];      // LSB
+  instruction_.update_instruction(instruction);
+}
+
+void CPU::execute_instruction() {
+  get_next_instruction();
+  uint8_t opc = instruction_.get_opc();
+
+  switch (opc) {
+  case 0x0: {
+    uint16_t nn = instruction_.get_nn();
+    if (nn == 0xEE) {
+      execute_ret();
+    } else if (nn == 0xE0) {
+      execute_cls();
+    }
+    break;
+  }
+  case 0x1: {
+    execute_jp_addr();
+    break;
+  }
+  case 0x2: {
+   execute_call_addr();
+   break;
+  }
+  default:
+    break;
+  }
+}
+
+void CPU::execute_jp_addr() { program_counter_ = instruction_.get_nnn(); }
+
+void CPU::execute_call_addr() {
+  if (stack_pointer_ >= STACK_SIZE) {
+    throw std::runtime_error(
+        "Stack overflow: Maximum nested subroutine depth exceeded.");
+  }
+  stack_[stack_pointer_] = program_counter_;
+  stack_pointer_++;
+  uint16_t nnn = instruction_.get_nnn();
+  program_counter_ = nnn;
+}
+
+void CPU::execute_ret() {
+  if (stack_pointer_ == 0) {
+    throw std::runtime_error(
+        "Stack underflow: Attempted to return without an active subroutine.");
+  }
+  stack_pointer_--;
+  program_counter_ = stack_[stack_pointer_];
 }
--- emulator-6502/src/cpu/cpu.h
+++ emulator-6502/src/cpu/cpu.h
@@ -2,6 +2,24 @@
 #include <array>
 #include <cstdint>
 
+template <typename T> T extract_bits(T input, uint8_t start, uint8_t length);
+
+struct Instruction {
+private:
+  uint16_t data = 0;
+
+public:
+  Instruction();
+  Instruction(uint16_t inst) : data(inst) {};
+  void update_instruction(uint16_t inst) { data = inst; };
+  const uint16_t get_x() { return extract_bits(data, 0x8, 0x4); };
+  const uint16_t get_y() { return extract_bits(data, 0x4, 0x4); };
+  const uint16_t get_n() { return extract_bits(data, 0x0, 0x4); };
+  const uint16_t get_nn() { return extract_bits(data, 0x0, 0x8); };
+  const uint16_t get_nnn() { return extract_bits(data, 0x0, 0xC); };
+  const uint16_t get_opc() { return extract_bits(data, 0xC, 0x4); };
+};
+
 class CPU {
 private:
   static constexpr std::size_t REGISTER_SIZE = 16;
@@ -19,6 +37,13 @@ private:
   uint16_t index_register_{INDEX_REGISTER_START};
   uint16_t program_counter_{PROGRAM_COUNTER_START};
   uint8_t stack_pointer_{STACK_POINTER_START};
+
+  Instruction instruction_;
+  void get_next_instruction();
+  void execute_cls(); // TODO
+  void execute_jp_addr();
+  void execute_call_addr();
+  void execute_ret();
 
 public:
   CPU();
