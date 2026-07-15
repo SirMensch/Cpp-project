41bd83c3aca9d1a24639b7df4a1078973116f736
SirMensch <larslue@googlemail.com>
2026-07-15 17:06:51 +0900

    ADD: ALU & skip equal stuff

--- emulator-6502/src/cpu/cpu.cpp
+++ emulator-6502/src/cpu/cpu.cpp
@@ -1,13 +1,13 @@
 #include "cpu.h"
 #include <cstdint>
 #include <stdexcept>
+#include <sys/types.h>
 
-template <typename T> T extract_bits(T input, uint8_t start, uint8_t length) {
+template <uint8_t start, uint8_t length, typename T> T extract_bits(T input) {
   // compile time assess
   constexpr uint8_t type_bits = sizeof(T) * 8;
   static_assert(std::is_integral<T>::value, "Template type must be an integer");
-  static_assert(start < type_bits, "Start position exceeds data type size");
-  static_assert(length <= type_bits, "Length exceeds data type size");
+  static_assert(start + length <= type_bits, "Length exceeds data type size");
 
   if (start >= type_bits || length == 0) {
     return 0;
@@ -44,30 +44,56 @@ void CPU::get_next_instruction() {
 
 void CPU::execute_instruction() {
   get_next_instruction();
-  uint8_t opc = instruction_.get_opc();
+  OPC opc = instruction_.get_opc();
 
   switch (opc) {
-  case 0x0: {
-    uint16_t nn = instruction_.get_nn();
-    if (nn == 0xEE) {
-      execute_ret();
-    } else if (nn == 0xE0) {
-      execute_cls();
-    }
+  case OPC::SYS: {
+    execute_sys();
     break;
   }
-  case 0x1: {
+  case OPC::JP: {
     execute_jp_addr();
     break;
   }
-  case 0x2: {
-   execute_call_addr();
-   break;
+  case OPC::CALL: {
+    execute_call_addr();
+    break;
+  }
+  case OPC::SE: {
+    execute_skip_eq();
+    break;
+  }
+  case OPC::SNE: {
+    execute_skip_not_eq();
+    break;
+  }
+  case OPC::LD: {
+    execute_ld();
+    break;
+  }
+  case OPC::ADD: {
+    execute_add();
+    break;
+  }
+  case OPC::R2RM: {
+    execute_alu();
+    break;
   }
   default:
     break;
   }
 }
+
+void CPU::execute_sys() {
+  uint16_t nn = instruction_.get_nn();
+  if (nn == 0xEE) {
+    execute_ret();
+  } else if (nn == 0xE0) {
+    execute_cls();
+  }
+}
+
+void CPU::execute_cls() {} // TODO
 
 void CPU::execute_jp_addr() { program_counter_ = instruction_.get_nnn(); }
 
@@ -89,4 +115,58 @@ void CPU::execute_ret() {
   }
   stack_pointer_--;
   program_counter_ = stack_[stack_pointer_];
+}
+
+void CPU::execute_ld() {
+  uint8_t x = instruction_.get_x();
+  uint8_t nn = instruction_.get_nn();
+  registers_[x] = nn;
+}
+
+void CPU::execute_add() {
+  uint8_t x = instruction_.get_x();
+  uint8_t nn = instruction_.get_nn();
+  registers_[x] += nn;
+}
+
+void CPU::execute_skip_eq() {
+  uint8_t x = instruction_.get_x();
+  uint8_t nn = instruction_.get_nn();
+  if (registers_[x] == nn) {
+    program_counter_ += 2;
+  }
+}
+
+void CPU::execute_skip_not_eq() {
+  uint8_t x = instruction_.get_x();
+  uint8_t nn = instruction_.get_nn();
+  if (registers_[x] != nn) {
+    program_counter_ += 2;
+  }
+}
+
+void CPU::execute_alu() {
+  uint8_t x = instruction_.get_x();
+  uint8_t y = instruction_.get_y();
+  ALU n = instruction_.get_n();
+  switch (n) {
+  case ALU::LD: {
+    registers_[x] = registers_[y];
+    break;
+  }
+  case ALU::OR: {
+    registers_[x] |= registers_[y];
+    break;
+  }
+  case ALU::AND: {
+    registers_[x] &= registers_[y];
+    break;
+  }
+  case ALU::XOR: {
+    registers_[x] ^= registers_[y];
+    break;
+  }
+  default:
+    break;
+  }
 }
--- emulator-6502/src/cpu/cpu.h
+++ emulator-6502/src/cpu/cpu.h
@@ -2,7 +2,25 @@
 #include <array>
 #include <cstdint>
 
-template <typename T> T extract_bits(T input, uint8_t start, uint8_t length);
+template <uint8_t start, uint8_t length, typename T> T extract_bits(T input);
+
+enum class OPC {
+  SYS = 0x0,  // system
+  JP = 0x1,   // jump
+  CALL = 0x2, // call address
+  SE = 0x3,   // skip if equal
+  SNE = 0x4,  // skip if not equal
+  LD = 0x6,   // load const in reg
+  ADD = 0x7,  // add to reg
+  R2RM = 0x8  // reg to reg math
+};
+
+enum class ALU {
+  LD = 0x0,  // load reg into reg
+  OR = 0x1,  // bitwise or
+  AND = 0x2, // bitwise and
+  XOR = 0x3, // bitwise xor
+};
 
 struct Instruction {
 private:
@@ -12,12 +30,12 @@ public:
   Instruction();
   Instruction(uint16_t inst) : data(inst) {};
   void update_instruction(uint16_t inst) { data = inst; };
-  const uint16_t get_x() { return extract_bits(data, 0x8, 0x4); };
-  const uint16_t get_y() { return extract_bits(data, 0x4, 0x4); };
-  const uint16_t get_n() { return extract_bits(data, 0x0, 0x4); };
-  const uint16_t get_nn() { return extract_bits(data, 0x0, 0x8); };
-  const uint16_t get_nnn() { return extract_bits(data, 0x0, 0xC); };
-  const uint16_t get_opc() { return extract_bits(data, 0xC, 0x4); };
+  uint16_t get_x() { return extract_bits<0x8, 0x4>(data); };
+  uint16_t get_y() { return extract_bits<0x4, 0x4>(data); };
+  ALU get_n() { return static_cast<ALU>(extract_bits<0x0, 0x4>(data)); };
+  uint16_t get_nn() { return extract_bits<0x0, 0x8>(data); };
+  uint16_t get_nnn() { return extract_bits<0x0, 0xC>(data); };
+  OPC get_opc() { return static_cast<OPC>(extract_bits<0xC, 0x4>(data)); };
 };
 
 class CPU {
@@ -38,12 +56,18 @@ private:
   uint16_t program_counter_{PROGRAM_COUNTER_START};
   uint8_t stack_pointer_{STACK_POINTER_START};
 
-  Instruction instruction_;
+  Instruction instruction_ = Instruction(0x0);
   void get_next_instruction();
+  void execute_sys();
   void execute_cls(); // TODO
   void execute_jp_addr();
   void execute_call_addr();
+  void execute_skip_eq();
+  void execute_skip_not_eq();
   void execute_ret();
+  void execute_ld();
+  void execute_add();
+  void execute_alu();
 
 public:
   CPU();
